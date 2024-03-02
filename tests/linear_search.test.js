9aed6b5f3257b4add40f194e2e1ab707e4d00f07
Armando <72764913+Armando284@users.noreply.github.com>
2024-03-02 03:03:15 -0500

    Feat: Add falsy test to linear search

--- tests/linear_search.test.js
+++ tests/linear_search.test.js
@@ -4,3 +4,7 @@ import { linearSearch } from '../src/linear_search'
 test('Find 3 in an array of [1,2,3,4]', () => {
   expect(linearSearch([1, 2, 3, 4], 3)).toBe(true)
 })
+
+test('Don\'t find 5 in [1,2,3,4]', () => {
+  expect(linearSearch([1, 2, 3, 4], 5)).toBeFalsy()
+})
