b838fe130dcc2766ac5978160ad30db545736445
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-08 10:25:21 -0400

    Feat: Formats code with new code style standar

--- tests/linear_search.test.js
+++ tests/linear_search.test.js
@@ -5,6 +5,6 @@ test('Find 3 in an array of [1,2,3,4]', () => {
   expect(linearSearch([1, 2, 3, 4], 3)).toBe(true)
 })
 
-test('Don\'t find 5 in [1,2,3,4]', () => {
+test("Don't find 5 in [1,2,3,4]", () => {
   expect(linearSearch([1, 2, 3, 4], 5)).toBeFalsy()
 })
