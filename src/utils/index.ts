813186860c59c6a07a8436fd1f17e2ecb4014985
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-09 19:37:11 -0400

    Feat: Adds a nullable value checker function to utils
    Sadly TypeScript and EsLint validations make difficult to work woth functions to validate null or undefined values so a lot of repeated logic for now 😒

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,6 @@ export const byteSize = (data: any): number => {
   const str = typeof data === 'string' ? data : JSON.stringify(data)
   return new Blob([str]).size
 }
+
+export const hasValue = (value: any): boolean =>
+  value !== undefined && value !== null
