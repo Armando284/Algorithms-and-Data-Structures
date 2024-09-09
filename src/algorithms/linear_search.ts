798b5d9b1b8f7717d75645a6c91253d1c538621a
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-09 16:12:15 -0400

    Feat: Adds linear search algorithm

--- src/algorithms/linear_search.ts
+++ src/algorithms/linear_search.ts
@@ -1,4 +1,4 @@
-export function linearSearch(haystack: number[], needle: number): boolean {
+export function linearSearch (haystack: number[], needle: number): boolean {
   let response = false
   for (let i = 0; i < haystack.length; i++) {
     if (haystack[i] === needle) {
