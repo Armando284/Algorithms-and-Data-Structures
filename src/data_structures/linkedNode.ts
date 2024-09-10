ec1c90a497a8efe49027c637182fda57518f4534
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-10 09:45:26 -0400

    Fix: Removes unnecesary parameters and methods from node class

--- src/data_structures/linkedNode.ts
+++ src/data_structures/linkedNode.ts
@@ -4,31 +4,23 @@ export interface LinkedNodeEntry<T> {
 }
 
 export class LinkedNode<T> {
-  private readonly _key?: number
   private readonly _value: T
   prev?: LinkedNode<T>
   next?: LinkedNode<T>
 
-  constructor ({ key, value }: LinkedNodeEntry<T>) {
-    this._key = key
+  constructor(value: T) {
     this._value = value
     this.prev = undefined
     this.next = undefined
   }
 
-  get key (): number | undefined {
-    return this._key
-  }
-
-  get value (): T {
+  get value(): T {
     return this._value
   }
 
-  toString (fn?: (value: any) => string): string {
+  toString(fn?: (value: LinkedNode<T>) => string): string {
     return fn !== null && fn !== undefined && typeof fn === 'function'
       ? fn(this)
-      : `{ ${
-          this.key !== undefined ? this.key.toString() + ': ' : ''
-        }${JSON.stringify(this.value)} }`
+      : `{ ${JSON.stringify(this.value)} }`
   }
 }
