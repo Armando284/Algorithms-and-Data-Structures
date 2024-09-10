323ee4520c14852cec291ed910d4bb5032585e23
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-09 22:54:04 -0400

    Feat: Adds a queue

---
+++ src/data_structures/queue.ts
@@ -0,0 +1,42 @@
+import { LinkedNode } from './linkedNode'
+
+export class Queue<T> {
+  public length: number
+  private head?: LinkedNode<T>
+  private tail?: LinkedNode<T>
+
+  constructor() {
+    this.head = undefined
+    this.tail = undefined
+    this.length = 0
+  }
+
+  enqueue(item: T): void {
+    this.length++
+    const node = new LinkedNode<T>({ value: item })
+    if (!this.head || !this.tail) {
+      this.head = node
+      this.tail = node
+      return
+    }
+
+    this.tail.next = node
+    this.tail = node
+  }
+
+  dequeue(): T | undefined {
+    if (!this.head) {
+      return
+    }
+
+    this.length--
+    const node = this.head
+    this.head = this.head.next
+    node.next = undefined // Here the previous head is entirely detached so it's on the hands of garbage collector
+    return node.value
+  }
+
+  peek(): T | undefined {
+    return this.head?.value
+  }
+}
