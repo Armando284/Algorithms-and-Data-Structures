c9228bb4f17a3ccb930c23dbdc9834c49c0728d3
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-28 17:00:34 -0400

    Feat: Add binary tree comparison
    Uses DFS for comparing value AND shape 🌟

---
+++ src/algorithms/binary_tree_comparison.ts
@@ -0,0 +1,24 @@
+/**
+ * Compare two binary trees in value AND shape
+ */
+
+import { BinaryNode } from '@/interfaces/nodes'
+
+export default function compare (
+  a: BinaryNode<number> | null,
+  b: BinaryNode<number> | null
+): boolean {
+  if (a === null && b === null) {
+    return true
+  }
+
+  if (a === null || b === null) {
+    return false
+  }
+
+  if (a.value !== b.value) {
+    return false
+  }
+
+  return compare(a.left, b.left) && compare(a.right, b.right)
+}
